c5a8a9b16d92813db46ada8ae6493ae974b3737d
Daniel Pérez <78990630+dpstack@users.noreply.github.com>
2022-11-08 04:53:26 -0500

    URL

--- src/api/clientes.js
+++ src/api/clientes.js
@@ -1,4 +1,4 @@
-const url = import.meta.env.VITE_API_URL;
+const url = 'https://my-json-server.typicode.com/dpstack/crm_clientes/clientes';
 
 export async function getClientes () {
     const response = await fetch(url);
